396c95b0724b373969fd4ad392eff2b98f753940
Miguel Alexandre <m.alexandrex@gmail.com>
2016-10-19 15:42:58 +0100

    Added functionality to create/remove/check existance of virtual directories.

---
+++ src/IIS/Settings/VirtualDirectorySettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core.IO;
+
+namespace Cake.IIS
+{
+    public class VirtualDirectorySettings : IDirectorySettings
+    {
+        #region Constructor (1)
+            public VirtualDirectorySettings()
+            {
+
+            }
+        #endregion
+
+        #region Properties (8)
+
+            public string ComputerName { get; set; }
+
+            public DirectoryPath PhysicalDirectory { get; set; }
+
+            public DirectoryPath WorkingDirectory { get; set; }
+            public string SiteName { get; set; }
+
+            public string ApplicationPath { get; set; }
+
+            public string Path { get; set; }
+
+            public AuthenticationSettings Authentication { get; set; }
+
+            public AuthorizationSettings Authorization { get; set; }
+
+        #endregion
+
+
+    }
+}
